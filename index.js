1bdffb87f77418c0ad76ae98d5deac3c5ce57031
stacy <stacydn23@gmail.com>
2021-05-22 23:18:40 -0400

    commit tree 30796e80d9994f2f3ba6257114b43f20038a89c6

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+// a variable called multiply whose results will equal 62
+const num1 = 31, num2 = 2
+const multiply = num1 * num2
+
+// a variable called random which generated a random interger greater than 0
+const random = Math.floor(Math.random() * 1000 ) + 1
+
+// a variable called mod set to an equation whose remainder is equal to 4
+const num3 = 16, num4 = 6
+const mod = num3 % num4
+
+// a variable called max whose result will return 20 as the highest number 
+const max = Math.max(5, 10, 15, 20)
